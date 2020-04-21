70b33bac9da1bd71c9c049fbef9eefa1bd059738
David Liptai <david.liptai@monash.edu>
2020-04-21 10:25:33 +1000

    initial commit

---
+++ cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clean up leftover build files
+rm -fr /home/*/{.ssh,.ansible,.cache}
+rm -fr /root/{.ssh,.ansible,.cache}
+rm -fr /root/'~'*
+
+# Truncate any log files
+find /var/log -type f -print0 | xargs -0 truncate -s0
+
+# Clean up Ubuntu user (but don't remove home directory)
+userdel -f ubuntu || true
+
+# Writes zeroes to empty space on the volume;
+# (allows for better compression)
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# synchronize corresponding file data in volatile memory and permanent storage;
+# (forces immediate execution of pending reads and writes)
+sync
