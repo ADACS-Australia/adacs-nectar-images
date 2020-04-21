e0f75303bdc99063ef15a911a49b916f997895c5
David Liptai <david.liptai@monash.edu>
2020-04-21 14:10:10 +1000

    added print statements to build script to indicate progress

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -e
 
+echo "Building image..."
 packer build packer.json
 
+echo "Saving image locally..."
 openstack --os-cloud=swin-dev image save --file packer-test_large.qcow2 packer-test
+
+echo "Deleting image on openstack..."
 openstack --os-cloud=swin-dev image delete packer-test
 
+echo "Compressing image..."
 qemu-img convert -c -o compat=0.10 -O qcow2 packer-test_large.qcow2 packer-test_small.qcow2
 rm packer-test_large.qcow2
 
-openstack --os-cloud=swin-dev image create --file packer-test_small.qcow2 packer-test
+echo "Uploading compressed image to openstack..."
+openstack --os-cloud=swin-dev image create --disk-format qcow2 --container-format bare --file packer-test_small.qcow2 packer-test
