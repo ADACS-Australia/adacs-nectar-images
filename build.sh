79f466f07d900dda2b81e87329ad1239530aafbf
David Liptai <david.liptai@monash.edu>
2020-04-22 12:27:23 +1000

    print commands as they're run; delete image after upload; fix bug in unset image properties

--- build.sh
+++ build.sh
@@ -28,6 +28,8 @@ fi
 FILE=packer.json
 IMAGE_NAME=$(jq -r '.builders[0].image_name' ${FILE})
 
+set -x
+
 echo "Building and provisioning image with: packer build ${FILE}..."
 packer build ${FILE}
 
@@ -43,11 +45,13 @@ rm ${IMAGE_NAME}_large.qcow2
 
 echo "Uploading compressed image to openstack..."
 openstack image create --disk-format qcow2 --container-format bare --file ${IMAGE_NAME}_small.qcow2 ${IMAGE_NAME}
+rm ${IMAGE_NAME}_small.qcow2
+
 openstack image set --property default_user=ubuntu ${IMAGE_NAME}
 openstack image set --property nectar_name=${IMAGE_NAME} ${IMAGE_NAME}
 openstack image set --property os_distro=ubuntu ${IMAGE_NAME}
 openstack image set --property os_version=18.04 ${IMAGE_NAME}
 
-openstack image unset --property owner_specified.openstack.sha256
-openstack image unset --property owner_specified.openstack.object
-openstack image unset --property owner_specified.openstack.md5
+openstack image unset --property owner_specified.openstack.sha256 ${IMAGE_NAME}
+openstack image unset --property owner_specified.openstack.object ${IMAGE_NAME}
+openstack image unset --property owner_specified.openstack.md5 ${IMAGE_NAME}
