e392036c5e7e5f3db2a5da2af17161d2003ea40e
David Liptai <david.liptai@monash.edu>
2020-04-22 12:00:54 +1000

    retrieve image name from packer file using jq

--- build.sh
+++ build.sh
@@ -1,17 +1,40 @@
 #!/bin/bash -e
 
-echo "Building image..."
-packer build packer.json
+# Find packer
+if ! hash packer >/dev/null 2>&1; then
+    echo "You need packer installed to use this script"
+    exit 1
+fi
+
+# Find jq
+if ! hash jq >/dev/null 2>&1; then
+    echo "You need jq installed to use this script"
+    exit 1
+fi
+
+FILE=packer.json
+IMAGE_NAME=$(jq -r '.builders[0].image_name' ${FILE})
+
+echo "Building and provisioning image with: packer build ${FILE}..."
+packer build ${FILE}
 
 echo "Saving image locally..."
-openstack --os-cloud=swin-dev image save --file packer-test_large.qcow2 packer-test
+openstack image save --file ${IMAGE_NAME}_large.qcow2 ${IMAGE_NAME}
 
 echo "Deleting image on openstack..."
-openstack --os-cloud=swin-dev image delete packer-test
+openstack image delete ${IMAGE_NAME}
 
 echo "Compressing image..."
-qemu-img convert -c -o compat=0.10 -O qcow2 packer-test_large.qcow2 packer-test_small.qcow2
-rm packer-test_large.qcow2
+qemu-img convert -c -o compat=0.10 -O qcow2 ${IMAGE_NAME}_large.qcow2 ${IMAGE_NAME}_small.qcow2
+rm ${IMAGE_NAME}_large.qcow2
 
 echo "Uploading compressed image to openstack..."
-openstack --os-cloud=swin-dev image create --disk-format qcow2 --container-format bare --file packer-test_small.qcow2 packer-test
+openstack image create --disk-format qcow2 --container-format bare --file ${IMAGE_NAME}_small.qcow2 ${IMAGE_NAME}
+openstack image set --property default_user=ubuntu ${IMAGE_NAME}
+openstack image set --property nectar_name=${IMAGE_NAME} ${IMAGE_NAME}
+openstack image set --property os_distro=ubuntu ${IMAGE_NAME}
+openstack image set --property os_version=18.04 ${IMAGE_NAME}
+
+openstack image unset --property owner_specified.openstack.sha256
+openstack image unset --property owner_specified.openstack.object
+openstack image unset --property owner_specified.openstack.md5
