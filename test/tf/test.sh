c42550769cd370ddc70557ce64cd034d8287b133
David Liptai <dliptai@swin.edu.au>
2020-06-22 21:21:33 +1000

    added test via terraform

---
+++ test/tf/test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+
+# This script requires:
+#  - Terraform
+#  - Chef InSpec
+#  - OpenStack credentials loaded in your environment
+
+# Find packer
+if ! hash terraform >/dev/null 2>&1; then
+    echo "You need terraform installed to use this script"
+    exit 1
+fi
+
+# Find inspec
+if ! hash inspec >/dev/null 2>&1; then
+    echo "You need inspec installed to use this script"
+    exit 1
+fi
+
+# Check if OpenStack credentials are loaded
+if [ -z "${OS_USERNAME}" ]; then
+    echo -e "Please load the OpenStack credentials! \n"
+    echo    "(source your OpenStack RC file)"
+    exit 1
+fi
+
+# Set variables
+source ../../vars.sh
+
+# Check if volumes are present and available
+STATUS=$(openstack volume show -c status -f value ${MATLAB_VOLUME})
+if [ "${STATUS}" != "available" ]; then
+  echo The volume \'"${MATLAB_VOLUME}"\' is "$STATUS"
+  openstack volume show "${MATLAB_VOLUME}"
+  exit 1
+fi
+
+# Export terraform variables
+export TF_VAR_test_image_name=${TEST_IMAGE}
+export TF_VAR_matlab_volume=$(openstack volume show -c id -f value ${MATLAB_VOLUME})
+
+# Launch test server
+terraform init
+terraform apply -auto-approve -backup=-
+
+# Save private key and IP address
+terraform output private_key > temporary_key.pem
+IP=$(terraform output IP)
+
+# Run tests
+inspec exec ../inspec-test -t ssh://${DEFAULT_USER}@${IP} -i temporary_key.pem || true
+
+# Cleanup
+terraform destroy -auto-approve -backup=-
+rm temporary_key.pem
+rm terraform.tfstate
---
+++ test/tf/test.tf
@@ -0,0 +1,51 @@
+# Set some variables
+variable "test_image_name" {
+  default = ""
+}
+variable "matlab_volume" {
+  default = ""
+}
+
+# Configure the OpenStack Provider
+provider "openstack" { version = "~> 1.28" }
+
+# Create temporary ssh key pair
+resource "openstack_compute_keypair_v2" "test-keypair" {
+  name = "adacs-image-testing-key"
+}
+
+# Launch VM with image to test
+resource "openstack_compute_instance_v2" "test-server" {
+  name            = "ADACS_IMAGE_TESTING"
+  image_name      = var.test_image_name
+  flavor_name     = "m3.small"
+  key_pair        = openstack_compute_keypair_v2.test-keypair.name
+  security_groups = ["default", "SSH"]
+
+# Wait for ssh connection
+  provisioner "remote-exec" {
+    inline = ["echo"]
+
+    connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = openstack_compute_keypair_v2.test-keypair.private_key
+    host        = openstack_compute_instance_v2.test-server.access_ip_v4
+    }
+  }
+}
+
+resource "openstack_compute_volume_attach_v2" "test-volume" {
+  instance_id = openstack_compute_instance_v2.test-server.id
+  volume_id   = var.matlab_volume
+}
+
+# Output varialbes
+output "private_key" {
+  sensitive = true
+  value = openstack_compute_keypair_v2.test-keypair.private_key
+}
+
+output "IP" {
+  value = openstack_compute_instance_v2.test-server.access_ip_v4
+}
