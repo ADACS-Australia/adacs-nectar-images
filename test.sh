ac3ad16e2e422429cb7f05d6602d87b2d2fc766e
David Liptai <dliptai@swin.edu.au>
2020-08-18 18:58:48 +1000

    pass INSPEC_CONTROLS through to packer to inspec provisioner

--- test.sh
+++ test.sh
@@ -17,25 +17,23 @@ if [ -z "${OS_USERNAME}" ]; then
     exit 1
 fi
 
+set -u
 PACKER_TEMPLATE=packer_test.json
 
 # Set variables
 source vars.sh
+echo "Testing image: ${NEW_IMAGE_NAME}"
+echo "using inspec controls: ${INSPEC_CONTROLS}"
 
-# Pass additional scrip arguments/options through to packer build
-PACKER_OPTS=$1
-
-# Set some directories
-VARS_DIR="ansible/vars/"
-INSPEC_DIR="inspec/"
+# Set some variables
+export INSPEC_PROFILE="inspec/"
+export INSPEC_VARSFILE="ansible/vars/conda_packages.yml"
 
 # Build and provision image
 packer build                                           \
   -color=false                                         \
-  -var "inspec_profile=${INSPEC_DIR}"                  \
-  -var "apt_packages=${VARS_DIR}/apt_packages.yml"     \
-  -var "conda_packages=${VARS_DIR}/conda_packages.yml" \
-  ${PACKER_OPTS} ${PACKER_TEMPLATE}
+  -var "inspec_profile=${INSPEC_PROFILE}"              \
+  ${PACKER_TEMPLATE}
 
 echo "========================================================"
 echo "END PACKER OUTPUT"
