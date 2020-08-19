4583a447a9e244425948520925c2b83cfc5b4ac5
David Liptai <dliptai@swin.edu.au>
2020-08-19 14:46:37 +1000

    update test_all script to keep count of failed tests

--- test_all.sh
+++ test_all.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -a
 
+err=0
 for IMG in ./image_vars/image_*; do
   source $IMG
   ./test.sh
+  err=$((err+$?))
 done
+
+echo ''
+echo '================================'
+if ! [ "$err" == "0" ]; then
+  echo "   FAILED TESTING ON ${err} IMAGES"
+else
+  echo "  PASSED TESTING ON ALL IMAGES"
+fi
+echo '================================'
