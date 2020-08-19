ff4e13f08377e5cb44be9ec879ccdadb05f5f7e4
David Liptai <dliptai@swin.edu.au>
2020-08-19 23:02:44 +1000

    github actions: wait until enough cores are available before running test.sh

--- openstack_check.sh
+++ openstack_check.sh
@@ -5,19 +5,30 @@ if ! hash nova >/dev/null 2>&1; then
     exit 1
 fi
 
-# Split the 'nova limits' line containing cores/vcpu usage info
-IFS='| ' read -r -a CORES <<< "$(nova limits | grep Cores)"
-
-CORES_USED=CORES[2]
-CORES_MAX=CORES[3]
-
+WAIT_TIME=30
+MAX_TRIES=60
+NTRIES=0
 CORES_NEEDED=2
-CORES_AVAILABLE=$((${CORES_MAX} - ${CORES_USED}))
 
-if ((  ${CORES_AVAILABLE} >= ${CORES_NEEDED} )); then
-  echo "There are $CORES_AVAILABLE cores available."
-  exit 0
-else
-  echo "NOT ENOUGH CORES AVAILABLE"
-  exit 1
-fi
+while (( ${NTRIES} < ${MAX_TRIES} )); do
+  NTRIES=$((${NTRIES}+1))
+
+  # Split the 'nova limits' line containing cores/vcpu usage info
+  IFS='| ' read -r -a CORES <<< "$(nova limits | grep Cores)"
+
+  CORES_USED=CORES[2]
+  CORES_MAX=CORES[3]
+  CORES_AVAILABLE=$((${CORES_MAX} - ${CORES_USED}))
+
+  if ((  ${CORES_AVAILABLE} >= ${CORES_NEEDED} )); then
+    echo "There are $CORES_AVAILABLE cores available."
+    exit 0
+  else
+    echo "NOT ENOUGH CORES AVAILABLE. Retrying in ${WAIT_TIME}s"
+    sleep ${WAIT_TIME}
+  fi
+
+done
+
+echo "TIMED OUT"
+exit 1
--- vars.sh
+++ vars.sh
@@ -9,7 +9,7 @@ OS_DISTRO='ubuntu'
 OS_VERSION='18.04'
 
 # Name to use for the temporary image during provisioning
-BUILD_NAME='ADACS_astro_image_build'
+BUILD_NAME='ADACS_image_build'
 
 # Name used for the image/server during testing
 TEST_NAME='TEST_'${BUILD_NAME}
